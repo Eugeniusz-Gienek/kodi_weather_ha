1dd48c2bb54cab57f30ef3f1a3bbce85740fc5e4
Eugeniusz-Gienek <eugene@sky.community>
2024-04-20 02:06:07 +0200

    Fix for datetime

--- lib/util.py
+++ lib/util.py
@@ -52,7 +52,7 @@ def utc_to_local(utc_dt):
     # get integer timestamp to avoid precision lost
     timestamp = calendar.timegm(utc_dt.timetuple())
     local_dt = datetime.fromtimestamp(timestamp)
-    assert utc_dt.resolution >= timedelta(microseconds=1)
+    assert utc_dt.resolution >= datetime.timedelta(microseconds=1)
     return local_dt.replace(microsecond=utc_dt.microsecond)
 
 def parse_date(event_date):
